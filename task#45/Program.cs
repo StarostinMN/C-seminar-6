0df769fb04309795cbf6ef5cf4fd7e214733c0d8
Mikhail Starostin <starostin.mihail@mail.ru>
2022-12-09 19:00:41 +0300

    add task#45 my option

---
+++ task#45/Program.cs
@@ -0,0 +1,37 @@
+﻿/* Задача 45: Напишите программу, которая будет создавать копию заданного массива с помощью поэлементного копирования. */
+Console.Clear();
+
+Console.WriteLine("Введите размерность массива");
+int size = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите минимальное допустимое значение в массиве");
+int min = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите максимальное допустимое значение в массиве");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[] array = FillArray(size, min, max);
+
+int[] FillArray(int sizeArray, int minValue, int maxValue)
+{
+    Random random = new Random();
+    int[] resultArray = new int[sizeArray];
+    for (int i = 0; i < resultArray.Length; i++)
+    {
+        resultArray[i] = random.Next(minValue, maxValue + 1);
+    }
+    return resultArray;
+}
+
+int[] CopyArray(int[] inputArray)
+{
+    int[] copyArray = new int[inputArray.Length];
+    for (int i = 0; i < inputArray.Length; i++)
+    {
+        copyArray[i] = inputArray[i];
+    }
+    return copyArray;
+}
+
+Console.WriteLine($"Исходный массив: {string.Join(",", array)}");
+Console.WriteLine($"Результатирующий массив: {string.Join(",", CopyArray(array))}");
